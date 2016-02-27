e307f671e0387c9ab9b1c059218c37af1b46ba8e
Peter Russu-Discultsu <petru.tristan@gmail.com>
2016-02-27 10:28:56 +0200

    new

---
+++ 2.2_advansed/blog_examp/js/main.js
@@ -0,0 +1,94 @@
+var App = App || {};
+
+App.main = (function () {
+	var container = document.getElementById('container');
+	var posts = [];
+	var comments = [];
+	postsJSON,
+	commentsJSON,
+	
+	getAllPostsAndComments();
+
+
+
+	xhrUtils.getAll("posts", getAllPostsAndComments, xhrUtils.failedRequest);
+
+	sortButton.addEventListener("click", function(){
+		postsJSON.sort();
+		posts = [];
+		container.innerHTML
+		renderAllPosts
+
+	});
+
+// function getAllPostsAndComments (postContent) {
+//   xhrUtils.getAll('comments', function (commentData){
+//    renderAllPosts(postContent, commentData);
+//   }, xhrUtils.failedRequest);  
+//  }
+
+	function getAllPosts
+
+
+	function getAllPostsAndComments (postContent) {
+		xhrUtils.getAll('comments', function (commentData){
+			renderAllPosts(postContent, commentData);
+		}, xhrUtils.failedRequest);		
+	}
+
+
+
+
+	function renderAllPosts (postContent, commentData) {
+		for (var i in postContent) {
+			createPost(postContent[i], commentData.filter(function (comment) {
+				return comment.postId === postContent[i].id;
+			}));
+		}
+
+		createPostBlock(function () {
+			xhrUtils.getAll("posts", getAllPostsAndRener, xhrUtils.failedRequest);
+		});
+	}
+
+	function createPostBlock () {
+		var head = document.createElement("input");
+		var body = document.createElement("input");
+		var create = document.createElement("button");
+		var wrap =  document.createElement("div");		
+		var title = document.createElement('h2');	
+		var labelName = document.createElement('label');
+		var labelBody = document.createElement('label');
+		labelName.innerHTML = "Title: "
+		labelBody.innerHTML = "Text: "
+		head.type = "text";
+		body.type = "textarea";
+		title.innerHTML = "Create new post";
+		create.innerHTML = "Create";
+		wrap.classList.add("newpost");
+		labelName.appendChild(head);
+		labelBody.appendChild(body);
+		wrap.appendChild(title);
+		wrap.appendChild(labelName);
+		wrap.appendChild(labelBody);
+		wrap.appendChild(create);
+		document.body.appendChild(wrap);
+
+		create.addEventListener("click", function () {
+			xhrUtils.create({body: body.value, postname: head.value }, "posts", function (data) {
+				createPost(data);
+			});			
+		});
+	}	
+
+	function createPost (data, comments) {
+		var newPost = new App.blog.Post(data, comments);
+		var self = this;
+
+
+
+		posts.push(newPost);
+		container.appendChild(newPost.element);
+	}	
+})();
+
